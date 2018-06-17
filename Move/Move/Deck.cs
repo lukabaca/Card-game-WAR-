8165ef93c3d68f213e56b8c89a82dd8a56462329
lukabaca <lkszblaszczyk@gmail.com>
2018-06-17 17:45:47 +0200

    making map

--- Move/Move/Deck.cs
+++ Move/Move/Deck.cs
@@ -12,7 +12,7 @@ namespace gameSpace
         //K - kier, P - pik, T - trefl, 
         private static List<String> gameDeck = new List<String>()
 
-        {"2_PIK", "3__PIK", "4__PIK", "5__PIK", "6__PIK", "7__PIK", "8__PIK", "9__PIK", "10__PIK", "J__PIK", "D__PIK", "K__PIK", "A__PIK",
+        {"2_PIK", "3_PIK", "4_PIK", "5_PIK", "6_PIK", "7_PIK", "8_PIK", "9_PIK", "10_PIK", "J_PIK", "D_PIK", "K_PIK", "A_PIK",
          "2_KIER", "3_KIER", "4_KIER", "5_KIER", "6_KIER", "7_KIER", "8_KIER", "9_KIER", "10_KIER", "J_KIER", "D_KIER", "K_KIER", "A_KIER"
 
         };
--- Move/Move/Game.cs
+++ Move/Move/Game.cs
@@ -8,6 +8,15 @@ namespace Move
 {
     class Game
     {
+        private static Dictionary<String, int> comparingMap = new Dictionary<string, int>()
+        {
+         {"2_PIK", 2}, { "3_PIK",3 }, { "4_PIK",4 }, { "5_PIK", 5 }, { "6_PIK", 6 }, { "7_PIK", 7 }, { "8_PIK",8 }, { "9_PIK",9 }, { "10_PIK",10 }, { "J_PIK", 11 }, { "D_PIK", 12 }, { "K_PIK", 13 }, { "A_PIK", 14 },
+         {"2_KIER", 2}, { "3_KIER",3 }, { "4_KIER",4 }, { "5_KIER", 5 }, { "6_KIER", 6 }, { "7_KIER", 7 }, { "8_KIER",8 }, { "9_KIER",9 }, { "10_KIER",10 }, { "J_KIER", 11 }, { "D_KIER", 12 }, { "K_KIER", 13 }, { "A_KIER", 14 }
+
+
+        };
+
+
         private List<String> currentDeck;
         //kupka kart, ktore wygralismy
         private List<String> wonCardsDeck;
